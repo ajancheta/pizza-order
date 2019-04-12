0001ed7d2ac334c77cfa7fae30d8d956af4d7159
AJ Ancheta <ashleyjancheta@gmail.com>
2019-04-12 10:45:30 -0700

    add object constructor for pizza order in js and add forms in html

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,29 @@
 // Business Interface Logic
 
+// Creating an object to store data of the user's name and personal choices on size and topping. This will be used to create a prototype to obtain total cost of pizza.
+function pizzaOrder(userName, size, topping) {
+  this.userName = userName,
+  this.size = size,
+  this.topping = topping
+}
 
+pizzaOrder.prototype.addTotal = function() {
+  var totalCost = ("$") + this.size + this.topping;
+  this.addTotal = totalCost;
+}
 
 // User Interface Logic
 $(document).ready(function(){
-$("button#nameButton").click(function(event) {
-  event.preventDefault();
+  $("button#nameButton").click(function(event) {
+    event.preventDefault();
+    var nameInput = $("input#userName").val();
+    $(".name").text(nameInput);
 
-  var nameInput = $("input#userName").val();
-  console.log("nameInput");
-  $(".name").text(nameInput);
+    });
 
-  })
+  $("#orderForm").submit(function(event){
+    event.preventDefault();
+    var userSize = parseInt($("#pizzaSize").val());
+    var userTopping = parseInt($(""))
+  });
 });
