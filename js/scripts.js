e9deceba37e1fc0992810f5de0c80470796a3951
AJ Ancheta <ashleyjancheta@gmail.com>
2019-04-12 14:03:14 -0700

    remove radio buttons and add form, add functionality to form in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,27 @@
 // Business Interface Logic ---------->
 
-// Creating an object to store collected pizza
-
+// Creating an object to store collected pizza info
 function userOrder(pizza, items) {
   this.pizza = [],
   this.items = 0
-}
+};
 
 function Pizza (userName, size, topping) {
-  this.id = 0,
+  this.id,
   this.userName = userName,
   this.size = size,
   this.topping = [topping]
-}
+};
 
 // Creating a prototype to add up collected data from the user and add total cost
-pizzaOrder.prototype.addNewPizza = function(size, topping) {
+userOrder.prototype.addToPizza = function(size, topping) {
   this.items += 1;
-  var userPizza = new Pizza (userName, size, topping);
+  var userPizza = new Pizza (size, topping);
   userPizza.id = this.items;
   this.pizza.push(userPizza);
 }
 
-pizzaOrder.prototype.findPizza = function(id) {
+userOrder.prototype.findPizza = function(id) {
   for (var i=0; i< this.pizza.length; i++) {
     if (this.pizza[i].id === id) {
       return this.pizza[i];
@@ -30,36 +29,32 @@ pizzaOrder.prototype.findPizza = function(id) {
   };
 };
 
-pizzaOrder.prototype.addPrice = function() {
+userOrder.prototype.addPrice = function() {
   var price = 0;
-  var pizza = userPizza.findPizza(pizzaId);
-  this.pizzas.forEach (function(pizza){
-    if (pizza.size === "Small (12\")") {
+  // var order = userOrder.findPizza(pizzaId);
+  this.pizza.forEach(function(pizza){
+    if (order.size === "Small (12\")") {
       price += 18;
-    } else if (pizza.size === "Large(18\")") {
+    } else if (order.size === "Large(18\")") {
       price += 20;
+
     }
-  })
-console.log("test");
-    for (var i=0; i < pizza.topping[0].length; i++) {
-      if (price === 20) {
+
+    for (i=0; i < pizzas.topping[0].length; i++) {
+      if (price >= 20) {
         price += 2;
-      } if (price === 18) {
+      } if (price >= 18) {
         price += 1;
       }
     }
-    return price;
-  };
-});
+  });
+  return price;
+};
 
-  // var totalCost = ("$") + (parseInt(this.size)) + (parseInt(this.topping));
-  // this.addTotal = totalCost;
-
-// Radio buttons submit values
-function selectSize(userSelects) {
-  if (userSelects === option1) {
-    return
-  }
+function showDetails (userOrder, pizzaId){
+  var order = userOrder.findPizza(pizzaId);
+  $(".size").html(order.size);
+  $(".topping").html(order.topping[0].join(", "));
 }
 
 // User Interface Logic ---------->
@@ -71,17 +66,24 @@ $(document).ready(function(){
 
     });
 
+// Submitting the order form
   $("#orderForm").submit(function(event){
     event.preventDefault();
     var nameInput = $("input#nameInput").val();
     var userSize = $("#pizzaSize").val();
     var userTopping = $("#pizzaTopping").val();
-    var submitType = $("input:radio[name=exampleRadios]:checked").val();
-    var finalOrder = new pizzaOrder(nameInput, userSize, userTopping);
+    var toppingInput = [];
+    $("input:checkbox[name=topping]:checked").each(function(){
+      topppingInput.push($(this).val());
+
+    });
+
+    var finalOrder = new userOrder(userSize, userTopping);
+    $(".total").text("$" + finalOrder);
     $(".name").text(nameInput);
     $(".size").text(userSize);
     $(".topping").text(userTopping);
-    finalOrder.addTotal();
-    $(".total").text(finalOrder.addTotal);
+    // finalOrder.addPrice();
+    // $(".total").text(finalOrder.addPrice);
   });
 });
